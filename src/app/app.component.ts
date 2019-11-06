2a5c004eacd5f41a648b148fb835132fc9a752a4
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-11-06 11:29:27 +0200

    add item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Item } from './item.interface';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,14 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title: string;
 
-  constructor() {
-    this.title = 'Hello Vulcan';
+
+  shoppingCart: Item[] = [];
+
+  items: Item[] =
+    [{
+      title: 'Our first item',
+      price: 25.5,
+      // tslint:disable-next-line
+      imgUrl: 'https://assets.adidas.com/images/w_600,f_auto,q_auto:sensitive,fl_lossy/34df30b8ae434fd88ed7a827007d01ec_9366/SST_Track_Jacket_Black_CW1256_21_model.jpg'
+    },
+    {
+      title: 'Iron from Ali Express',
+      price: 30,
+      // tslint:disable-next-line
+      imgUrl: 'https://images-na.ssl-images-amazon.com/images/I/8130-XR4zLL._SL1500_.jpg'
+    }];
+
+  addToCart(item) {
+    this.shoppingCart.push(item);
   }
 
-  changeTitle(){
-    this.title = 'Hello Yahav';
+  removeFromCart(item) {
+    // ToDO
   }
 }
 
